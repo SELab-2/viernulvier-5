bb32845104d1e280498c90cede1788f7d963536d
Jules <jules.bauwens1@gmail.com>
2026-03-09 18:42:00 +0100

    fix main scraper problem

--- backend/src/scraper/scraper.ts
+++ backend/src/scraper/scraper.ts
@@ -1016,12 +1016,12 @@ async function main() {
 
 }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect();
+//   })
+//   .catch(async (e) => {
+//     console.error(e);
+//     await prisma.$disconnect();
+//     process.exit(1);
+//   })
