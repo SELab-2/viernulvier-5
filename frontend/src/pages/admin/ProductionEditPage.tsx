6c006d45bd5f730197221c231361e4ef3a7ca284
fish <jan.minjuk@ugent.be>
2026-03-29 03:08:14 +0200

    feat: added component for production language tabs

--- frontend/src/pages/admin/ProductionEditPage.tsx
+++ frontend/src/pages/admin/ProductionEditPage.tsx
@@ -1,14 +1,39 @@
-import AdminProductionContentTab from '../../components/admin/ProductionContentTab'
+import AdminProductionContentTab from '../../components/admin/ProductionTabContent'
 import AdminProductionEditProps from '../../components/admin/ProductionEdit'
 import PublicLayout from '../../components/public/PublicLayout'
 import { getMessages } from '../../i18n'
+import ProductionEditHeader from '../../components/admin/ProductionEditHeader'
+import { useNavigate } from 'react-router-dom'
+import ProductionTab from '../../components/admin/ProductionTab'
+import { useState } from 'react'
 
 /**
  * Admin page for editing a pproduction item.
  */
 function ProductionEditPage() {
-
     const messages = getMessages()
+    const navigate = useNavigate()
+
+    // TODO: for now this is fine but later we want maybe variable length language tabs...
+    const [language, setLanguage] = useState('nl')
+
+    const back = () => {
+        navigate(-1)
+    }
+    
+    const saveAsDraft = () => {
+        // TODO: save as draft impl.
+    }
+
+    const publish = () => {
+        // TODO: publish impl.
+    }
+
+    const setTab = (key: string) => {
+        setLanguage(key)
+
+        // TODO: proper impl 
+    }
 
     return (
         // TODO: make a private layout for admins
@@ -17,12 +42,28 @@ function ProductionEditPage() {
             archiveLabel={messages.nav.archive}
             searchAriaLabel={messages.nav.searchAriaLabel}
             searchPlaceholder={messages.nav.searchPlaceholder}
-        >
+        >   
+            <ProductionEditHeader
+                backText={messages.production.back}
+                saveAsDraftText={messages.production.saveOnDraft}
+                publishText={messages.production.publish}
+                back={back}
+                saveAsDraft={saveAsDraft}
+                publish={publish}   
+            />
+
+
             <AdminProductionEditProps
                 editTitle={messages.production.editTitle}
                 editSubTitle={messages.production.editSubTitle}
             >
             </AdminProductionEditProps>
+
+            <ProductionTab
+                setTab={setTab}
+                language={language}
+            >
+            </ProductionTab>
 
             <AdminProductionContentTab
                 title={messages.production.title}
@@ -30,9 +71,6 @@ function ProductionEditPage() {
                 content={messages.production.content}
             >
             </AdminProductionContentTab>
-            
-             
-
         </PublicLayout>
     )
 }
