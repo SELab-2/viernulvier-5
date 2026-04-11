7294aeabc6255f08ef51e18f60e75ec89c2f7f34
saravlan <S.Vanlandeghem@UGent.be>
2026-04-12 01:46:33 +0200

    added saving location to .env for better saving later

--- backend/src/media/size_checker.ts
+++ backend/src/media/size_checker.ts
@@ -1,5 +1,7 @@
 import { prisma } from "./prisma";
 import axios from "axios";
+import path from 'path';
+import * as fs from "node:fs";
 
 
 
@@ -7,100 +9,6 @@ import axios from "axios";
 // and then grab all the media items used in these which leads us to the actually used crops.
 
 // update: there is not a single gallery in tags.
-
-
-async function get_media_items(){
-    const med = await prisma.item.findMany({});
-    console.log(med.length);
-
-
-
-    const productions = await prisma.production.findMany({
-        where: {
-            OR: [{
-                NOT: {
-                    media_gallery: null
-                },
-            }, {
-                NOT: {
-                    poster_gallery: null
-                },
-            },{
-                NOT: {
-                    review_gallery: null
-                }
-            }]
-        },
-        include: {
-            media_gallery: true,
-            poster_gallery: true,
-            review_gallery: true,
-        }
-    });
-
-    const galleries = new Set<any>();
-
-    for (const production of productions){
-        if (production.media_gallery) {
-            galleries.add(
-                await prisma.gallery.findUnique({
-                    where: {id: production.media_gallery_id!},
-                    include: {items: true}
-                }));
-        }
-        if (production.review_gallery) {
-            galleries.add(await prisma.gallery.findUnique({
-                where: {id: production.review_gallery_id!},
-                include: {items: true}
-            }));
-
-        }
-        if (production.poster_gallery) {
-            galleries.add(await prisma.gallery.findUnique({
-                where: {id: production.poster_gallery_id!},
-                include: {items: true}
-            }));
-        }
-    }
-
-    console.log("galleries:", galleries.size);
-    const media_items = [];
-    let index = 0;
-
-    for (const gallery of galleries){
-
-        for (const item of gallery.items){
-            const db_item = await prisma.item.findUnique({
-                where: {id: item.id,
-                    NOT: {
-                        crops: undefined,
-                    }
-                },
-                include: {crops: true}
-            });
-            if (db_item) {
-                media_items.push(db_item);
-            }
-        }
-        if (index % 100 === 0) {
-            console.log("index:", index);
-        }
-        index++;
-
-    }
-
-    console.log(media_items.length);
-
-    const crops = new Set<any>();
-    for (const item of media_items) {
-        for (const crop of item.crops){
-            crops.add(crop);
-        }
-    }
-    console.log("done");
-    return crops;
-}
-
 
 async function get_crops() {
     let crops = await prisma.crop.findMany({});
@@ -130,6 +38,11 @@ async function download_crop(crop: any) {
             method: 'GET',
             responseType: 'arraybuffer',
         });
+
+        const filename = `${crop.id}.jpg`;
+        const filepath = path.join(process.env.FILE_LOCATION!, filename);
+        fs.writeFileSync(filepath, response.data);
+        console.log(`Saved: ${filepath}`);
         return Number(response.headers['content-length']); // bytes
 
         // console.log("File size:", fileSize, "bytes");
