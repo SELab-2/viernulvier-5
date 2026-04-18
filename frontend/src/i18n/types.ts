840a87a4cd913a6fa98663b4da72642799b4760f
laveenae <lairdarthur.vaneenaeme@ugent.be>
2026-04-18 03:09:50 +0200

    use i18n

--- frontend/src/i18n/types.ts
+++ frontend/src/i18n/types.ts
@@ -122,6 +122,14 @@ export type Messages = {
   }
   detail: {
     navBackToOverview: string
+    dates: string
+    noEvents: string
+    date: string
+    time: string
+    location: string
+    showLess: string
+    showMore: string
+    credits: string
   }
   footer: {
     brandLogoAlt: string
