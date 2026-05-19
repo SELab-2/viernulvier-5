ba759e524cebd0c8ef9d6aaf41f1ac111b4038f6
saravlan <S.Vanlandeghem@UGent.be>
2026-05-19 22:15:27 +0200

    set production standard as non-draft for csv

---
+++ backend/prisma/migrations/20260519201014_draft_false/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `galleryId` on the `production` table. All the data in the column will be lost.
+  - You are about to drop the column `other_gallery_id` on the `production` table. All the data in the column will be lost.
+
+*/
+ALTER COLUMN "draft" SET DEFAULT false;
